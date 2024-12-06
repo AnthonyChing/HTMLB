a9b505debc25aafddd0f3577ef36acb50ea1bb7d
Anthony Ching <aching517@gmail.com>
2024-12-06 13:04:19 +0800

    Preprocessing - undrop column "season"

--- stage 1/preprocessing/drop.py
+++ stage 1/preprocessing/drop.py
@@ -1,10 +1,10 @@
 import pandas as pd
 df = pd.read_csv('reindex.csv')
-df = df.drop(columns=['id', 'home_team_abbr', 'away_team_abbr', 'date', 'is_night_game', 'home_pitcher', 'away_pitcher', 'season', 'home_team_season', 'away_team_season'])
+df = df.drop(columns=['id', 'home_team_abbr', 'away_team_abbr', 'date', 'is_night_game', 'home_pitcher', 'away_pitcher', 'home_team_season', 'away_team_season'])
 
 df.to_csv('train.csv', index=False)
 
 df = pd.read_csv('reindex_test.csv')
-df = df.drop(columns=['id', 'home_team_abbr', 'away_team_abbr', 'is_night_game', 'home_pitcher', 'away_pitcher', 'season', 'home_team_season', 'away_team_season'])
+df = df.drop(columns=['id', 'home_team_abbr', 'away_team_abbr', 'is_night_game', 'home_pitcher', 'away_pitcher', 'home_team_season', 'away_team_season'])
 
 df.to_csv('test.csv', index=False)
