0a234ce0e66861cd8aec3e22638975b7433c0f08
Anthony Ching <aching517@gmail.com>
2024-12-14 00:50:18 +0800

    Preprocessing - Added tscv.py to split data to time series cross validation format

---
+++ Anthony/preprocessing/tscv.py
@@ -0,0 +1,19 @@
+import pandas as pd
+df = pd.read_csv('../../uranus/preprocessing/undropped_train.csv')
+df['date'] = pd.to_datetime(df['date'])
+df = df.sort_values(by=['date'], ascending=[True])
+
+splits = 5
+N = len(df)
+n = int(N/splits)
+
+train_size = [i*n for i in range(1, splits)]
+
+for index, size in enumerate(train_size):
+    train = df.head(size)
+    if index == splits - 2:
+        test = df.iloc[size:N]
+    else:
+        test = df.iloc[size:size+n]
+    train.to_csv(f'../train_data_tscv_{index}.csv', index=False)
+    test.to_csv(f'../test_data_tscv_{index}.csv', index=False)
